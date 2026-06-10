e040930b1a49da58906838cffb5bb83e49446f6f
Alw1n-oss <Bespalov565@gmail.com>
2026-06-10 21:03:12 +0300

    Delete eventmanager-api directory

--- eventmanager-api/src/main/java/ru/hse/eventmanager/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.hse.eventmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.hse.eventmanager.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-}
--- eventmanager-api/src/main/java/ru/hse/eventmanager/repository/UserRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.hse.eventmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.hse.eventmanager.model.UserRole;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
-}
